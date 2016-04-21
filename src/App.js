ef59a4e40792a099475fcbf29d6935ac1bb54ef3
Alex Russell-Saw <alexrussellsaw@gmail.com>
2016-04-21 21:49:09 +0100

    tidy up sample component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hlo, werld.</h1>
+      <h1>Hello Electron</h1>
     );
   }
 }
